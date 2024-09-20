7894ee7c57c0f870ca9a9a5bdacf6cf203bb32d8
XyvinTech <141544499+XyvinTech@users.noreply.github.com>
2024-09-20 13:30:07 +0530

    test

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,27 +2,33 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "@svgr/rollup";
 
+// export default defineConfig({
+//   plugins: [react(), svgr()],
+//   define: {
+//     global: {},
+//   },
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: "http://52.66.173.34:3000",
+//         changeOrigin: true,
+//         secure: false, // Note: This allows insecure connections
+//         rewrite: (path) => path.replace(/^\/api/, ""),
+//       },
+//     },
+//   },
+//   build: {
+//     outDir: "dist",
+//   },
+//   resolve: {
+//     alias: {
+//       "./runtimeConfig": "./runtimeConfig.browser",
+//     },
+//   },
+// });
+
+
+
 export default defineConfig({
   plugins: [react(), svgr()],
-  define: {
-    global: {},
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://52.66.173.34:3000",
-        changeOrigin: true,
-        secure: false, // Note: This allows insecure connections
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
-  build: {
-    outDir: "dist",
-  },
-  resolve: {
-    alias: {
-      "./runtimeConfig": "./runtimeConfig.browser",
-    },
-  },
-});
+})
